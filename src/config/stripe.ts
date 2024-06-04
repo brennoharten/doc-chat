52f330710f21bfe98cfe0682667df9809a00b84f
brennoflorencio <brenno.florencio@aec.com.br>
2024-06-03 22:29:31 -0300

    aidicionando pagamento

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -18,9 +18,9 @@ export const PLANS = [
     quota: 50,
     pagesPerPdf: 25,
     price: {
-      amount: 14,
+      amount: 19.90,
       priceIds: {
-        test: 'price_1NuEwTA19umTXGu8MeS3hN8L',
+        test: 'price_1PNf8IIfElw2nfPo51ztiRqC',
         production: '',
       },
     },
